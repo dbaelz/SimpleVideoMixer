7efa60f7e052c70d0c17f8e73b6843321ca5a300
Daniel Bälz <info@dbaelz.de>
2025-09-09 13:57:09 +0200

    Also reformat audio models file

--- src/audio_models.py
+++ src/audio_models.py
@@ -2,16 +2,19 @@ from dataclasses import dataclass
 from typing import Optional, TypedDict
 from enum import Enum
 
+
 class AudioTrack(TypedDict):
     file: str
     volume: float
     delay: float
     repeat: int
 
+
 class SourceType(Enum):
     AUDIO = "audio"
     VIDEO = "video"
-    
+
+
 @dataclass
 class AudioSource:
     input_idx: int
